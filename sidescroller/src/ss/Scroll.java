2d4cd60e755dd960ec6d98646f2bdd6518944dc1
rharper <rharper@WS-I3-14.WS-ACADBUS.EDU>
2014-03-13 09:01:08 -0700

    init

---
+++ sidescroller/src/ss/Scroll.java
@@ -0,0 +1,28 @@
+package ss;
+
+import java.awt.Color;
+
+import jgame.GRootContainer;
+import jgame.Game;
+import jgame.ImageCache;
+
+public class Scroll extends Game{
+	public static void main(String[] args) {
+		ImageCache.create(Scroll.class, "/ss/rsc/");
+		Scroll s1 = new Scroll();
+		s1.startGame("Side Scroller");
+	}
+	
+	public Scroll() {
+		GRootContainer root = new GRootContainer(Color.BLACK);
+		setRootContainer(root);
+		
+		root.addView(Views.GAME, new ScrollGameView());
+		
+		setTargetFPS(30);
+	}
+	
+	public enum Views {
+		GAME
+	}
+}
---
+++ sidescroller/src/ss/ScrollGameView.java
@@ -0,0 +1,20 @@
+package ss;
+
+import jgame.GContainer;
+
+public class ScrollGameView extends GContainer {
+	
+	public ScrollGameView() {
+		setSize(1280, 720);
+	}
+	
+	public void viewShown() {
+		initScrollGameView();
+	}
+	
+	public void initScrollGameView() {
+		
+		
+	}
+
+}
